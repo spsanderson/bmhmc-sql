19bdd83979b7e66c4ae8e22b02749e2c00e297d9
Steven Paul Sanderson II <spsanderson@gmail.com>
2013-07-03 18:09:34 -0400

    update and expansion, fixed incorrect results to Internal Med stemming from incorrect call to outside From clause

--- ALCASABAS/PHYS_ALOS_AND_DEPT_ALOS.sql
+++ ALCASABAS/PHYS_ALOS_AND_DEPT_ALOS.sql
@@ -7,9 +7,9 @@ SET @ENDATE = '2013-05-31'
 SELECT DISTINCT pv.pract_rpt_name AS 'PHYSICIAN'
 , pv.med_staff_dept AS 'MED STAFF'
 , COUNT(DISTINCT vr.pt_id) AS '# PTS'
-, AVG(vr.len_of_stay) AS 'AVG LOS'
-, AVG(vr.drg_std_days_stay) AS 'AVG DRG LOS BENCH'
-, AVG(vr.len_of_stay - vr.drg_std_days_stay) AS 'OPPORTUNITY'
+, AVG(vr.len_of_stay) AS 'MD ALOS'
+, AVG(vr.drg_std_days_stay) AS 'MD ALOS BENCH'
+, AVG(vr.len_of_stay - vr.drg_std_days_stay) AS 'AVG OPP'
 , (SELECT
     COUNT(DISTINCT v.PT_ID) 
     FROM smsdss.pract_dim_v p
@@ -19,7 +19,7 @@ SELECT DISTINCT pv.pract_rpt_name AS 'PHYSICIAN'
     AND v.adm_dtime BETWEEN @STARTDATE AND @ENDATE
     AND v.vst_type_cd = 'I'
     AND p.spclty_desc != 'NO DESCRIPTION'
-    AND pv.spclty_desc NOT LIKE 'HOSPITALIST%'
+    AND p.spclty_desc NOT LIKE 'HOSPITALIST%'
     AND v.drg_std_days_stay IS NOT NULL
     AND p.pract_rpt_name != '?'
     AND p.orgz_cd = 's0x0'
@@ -29,7 +29,6 @@ SELECT DISTINCT pv.pract_rpt_name AS 'PHYSICIAN'
     ,'SURGERY'
     )
 ) AS '# PTS For Dept'
--- currently not working properly
 , (SELECT
     AVG(V.len_of_stay)
 	FROM smsmir.vst_rpt v
@@ -39,7 +38,7 @@ SELECT DISTINCT pv.pract_rpt_name AS 'PHYSICIAN'
     AND v.adm_dtime BETWEEN @STARTDATE AND @ENDATE
     AND v.vst_type_cd = 'I'
     AND p.spclty_desc != 'NO DESCRIPTION'
-    AND pv.spclty_desc NOT LIKE 'HOSPITALIST%'
+    AND p.spclty_desc NOT LIKE 'HOSPITALIST%'
     AND v.drg_std_days_stay IS NOT NULL
     AND p.pract_rpt_name != '?'
     AND p.orgz_cd = 's0x0'
@@ -49,6 +48,44 @@ SELECT DISTINCT pv.pract_rpt_name AS 'PHYSICIAN'
     'SURGERY'
     )
 ) AS 'DEPT ALOS'
+, (SELECT
+	AVG(v.drg_std_days_stay)
+	FROM smsmir.vst_rpt v
+	JOIN smsdss.pract_dim_v p
+	ON v.adm_pract_no = p.src_pract_no
+	WHERE pv.med_staff_dept = p.med_staff_dept
+	AND v.adm_dtime BETWEEN @STARTDATE AND @ENDATE
+    AND v.vst_type_cd = 'I'
+    AND p.spclty_desc != 'NO DESCRIPTION'
+    AND p.spclty_desc NOT LIKE 'HOSPITALIST%'
+    AND v.drg_std_days_stay IS NOT NULL
+    AND p.pract_rpt_name != '?'
+    AND p.orgz_cd = 's0x0'
+    AND p.med_staff_dept IN (
+    'INTERNAL MEDICINE',
+    'FAMILY PRACTICE',
+    'SURGERY'
+    )
+) AS 'DEPT BENCH'
+, (SELECT
+	AVG(v.len_of_stay - v.drg_std_days_stay)
+	FROM smsmir.vst_rpt v
+	JOIN smsdss.pract_dim_v p
+	ON v.adm_pract_no = p.src_pract_no
+	WHERE pv.med_staff_dept = p.med_staff_dept
+	AND v.adm_dtime BETWEEN @STARTDATE AND @ENDATE
+    AND v.vst_type_cd = 'I'
+    AND p.spclty_desc != 'NO DESCRIPTION'
+    AND p.spclty_desc NOT LIKE 'HOSPITALIST%'
+    AND v.drg_std_days_stay IS NOT NULL
+    AND p.pract_rpt_name != '?'
+    AND p.orgz_cd = 's0x0'
+    AND p.med_staff_dept IN (
+    'INTERNAL MEDICINE',
+    'FAMILY PRACTICE',
+    'SURGERY'
+    )
+) AS 'DEPT OPP'
 
 FROM smsmir.vst_rpt vr
 JOIN smsdss.pract_dim_v pv
@@ -63,8 +100,8 @@ AND pv.pract_rpt_name != '?'
 AND pv.orgz_cd = 's0x0'
 AND pv.med_staff_dept IN (
 'INTERNAL MEDICINE'
---,'FAMILY PRACTICE'
---,'SURGERY'
+,'FAMILY PRACTICE'
+,'SURGERY'
 )
 GROUP BY pv.pract_rpt_name, pv.med_staff_dept, pv.spclty_desc
 ORDER BY pv.med_staff_dept, AVG(vr.len_of_stay - vr.drg_std_days_stay)DESC
