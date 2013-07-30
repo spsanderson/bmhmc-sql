57bc813d3b17830afe1c6a7555393ddb65700a93
Steven Paul Sanderson II <spsanderson@gmail.com>
2013-07-30 16:21:53 -0400

    remove order by code

--- AMI/AMI_READMISSIONS.sql
+++ AMI/AMI_READMISSIONS.sql
@@ -1,4 +1,5 @@
 -- COPD DATA ON READMISSIONS FOR DRG 280, 281, 282
+
 --#####################################################################
 
 DECLARE @STARTDATE DATETIME
@@ -50,7 +51,6 @@ AND adm_src_desc != 'SCHEDULED ADMISSION'
 AND pt_no < 20000000
 AND B_Adm_Src_Desc != 'SCHEDULED ADMISSION'
 AND B_Pt_No < 20000000
-ORDER BY B_Adm_Date
 
 
 --#####################################################################
