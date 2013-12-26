caa5ce42f90d8bc9d136d2dc63e473da8bf7ee0b
Steven Paul Sanderson II <spsanderson@gmail.com>
2013-12-26 17:20:36 -0500

    add HEP C and SOUND MD 2 queries

---
+++ HEP_C.sql
@@ -0,0 +1,102 @@
+SET ANSI_NULLS OFF
+GO
+
+-- VARIABLE DECLARATION AND INITIALIZATION
+DECLARE @SD DATETIME;
+DECLARE @ED DATETIME;
+DECLARE @BD1 DATETIME;
+DECLARE @BD2 DATETIME;
+SET @SD = '2013-12-01';
+SET @ED = '2013-12-31';
+SET @BD1 = '1945-01-01';
+SET @BD2 = '1965-12-31';
+
+/*
+THIS QUERY WILL CREATE A TABLE THAT ALL OF THE PEOPLE WHO ARE BORN
+BETWEEN THE YEARS SPECIFIED WILL POPULATE
+*/
+-- TABLE DECLARATION
+DECLARE @T1 TABLE (
+[ENCOUNTER ID] VARCHAR(200)
+)
+-- WHAT GETS INSERTED INTO @T1
+INSERT INTO @T1
+SELECT
+A.[VISIT ID]
+
+-- FROM
+FROM (
+	SELECT DISTINCT PTNO_NUM AS [VISIT ID]
+	FROM smsdss.BMH_PLM_PtAcct_V
+	WHERE Pt_Birthdate BETWEEN @BD1 AND @BD2
+	AND Dsch_Date BETWEEN @SD AND @ED
+	AND Plm_Pt_Acct_Type = 'I'
+	AND PtNo_Num < '20000000'
+) A
+--SELECT * FROM @T1
+/*
+-----------------------------------------------------------------------
+END OF QUERY ONE (1)
+-----------------------------------------------------------------------
+*/
+--#####################################################################
+/*
+-----------------------------------------------------------------------
+START OF QUERY TWO (2)
+-----------------------------------------------------------------------
+*/
+-- @T2 DECLARATION
+DECLARE @T2 TABLE (
+[ENCOUNTER ID] VARCHAR(200)
+, [ORDER NUMBER] VARCHAR(200)
+, [ORDER DESC] VARCHAR(500)
+, [GOT HEP C ORDER] VARCHAR(200)
+)
+
+-- WHAT GETS INSERTED INTO @T2
+INSERT INTO @T2
+SELECT
+B.[ENCOUNTER ID]
+, B.[ORD NO]
+, B.[ORDER DESC]
+, B.ROWNUM
+
+-- WHERE IT ALL COMES FROM
+FROM (
+	SELECT EPISODE_NO AS [ENCOUNTER ID]
+	, ord_no AS [ORD NO]
+	, svc_desc AS [ORDER DESC]
+	, ROW_NUMBER() OVER(
+					PARTITION BY EPISODE_NO ORDER BY ORD_NO
+					) AS ROWNUM
+					
+	FROM smsmir.sr_ord
+	WHERE svc_desc IN (
+		'HEPATITIS C GENOTYPE'
+		, 'HEPATITIS C'
+		, 'HEPATITIS PANEL ( B AND C)'
+		)
+	AND episode_no < '20000000'
+) B
+
+WHERE B.ROWNUM = 1
+
+--SELECT * FROM @T2
+/*
+-----------------------------------------------------------------------
+END OF QUERY TWO (2)
+-----------------------------------------------------------------------
+*/
+--#####################################################################
+/*
+-----------------------------------------------------------------------
+PUTTING IT ALL TOGETHER NOW
+-----------------------------------------------------------------------
+*/
+SELECT T1.[ENCOUNTER ID]
+, ISNULL(T2.[ORDER DESC], 'NO HEP C ORDER') AS [ORDER DESC]
+, ISNULL(T2.[ORDER NUMBER], 'DID NOT GET HEP C ORDER') AS [HEP C ORDER]
+
+FROM @T1 T1
+LEFT JOIN @T2 T2
+ON T1.[ENCOUNTER ID] = T2.[ENCOUNTER ID]
---
+++ SOUND_MD_Q2.sql
@@ -0,0 +1,96 @@
+SET ANSI_NULLS OFF
+GO
+-- VARIABLE DECLARATION AND INITIALIZATION
+DECLARE @SD DATETIME;
+DECLARE @ED DATETIME;
+SET @SD = '2012-10-01';
+SET @ED = '2013-09-30';
+
+/*
+-----------------------------------------------------------------------
+THIS QUERY WILL GET ALL THE FRONT END INFORMATION REQUIRED FOR HOSIM
+AND INSERT IT INTO A TABLE THAT WILL GET MATCHED UP WITH THE DISCHARGE
+ORDERS TABLE
+-----------------------------------------------------------------------
+START OF QUERY 1
+-----------------------------------------------------------------------
+*/
+-- TABLE DECLARATION
+DECLARE @T1 TABLE (
+	[ENCOUNTER ID] VARCHAR(200) UNIQUE CLUSTERED
+)
+-- WHAT GETS INSERTED INTO @T1
+INSERT INTO @T1
+SELECT
+A.[VISIT ID]
+-- END @T1 INSERT SELECTION
+
+-- WHERE IT ALL COMES FROM
+-- COLUMN SELECTION
+FROM (
+	SELECT DISTINCT PAV.PtNo_Num AS [VISIT ID]
+
+	-- FROM DB(S)
+	FROM smsdss.BMH_PLM_PtAcct_V PAV
+	JOIN smsdss.pract_dim_v PDV
+	ON PAV.Adm_Dr_No = PDV.src_pract_no
+
+	-- FILTER(S)
+	WHERE PAV.Dsch_Date BETWEEN @SD AND @ED
+	AND PAV.Plm_Pt_Acct_Type = 'I'
+	AND PAV.PtNo_Num < '20000000'
+	AND PDV.src_spclty_cd = 'HOSIM'
+	AND PDV.orgz_cd = 'S0X0'
+) A
+/*
+-----------------------------------------------------------------------
+END OF QUERY ONE (1)
+-----------------------------------------------------------------------
+*/
+--#####################################################################
+/*
+-----------------------------------------------------------------------
+QUERY TWO (2)
+-----------------------------------------------------------------------
+*/
+-- TABLE @T2 DECLARATION
+DECLARE @T2 TABLE (
+	[ENCOUNTER ID] VARCHAR(200)
+	, [DX CD] VARCHAR(200)
+	, [DX PRIORITY CD] VARCHAR(200)
+)
+
+-- WHAT GETS INSERTED INTO @T2
+INSERT INTO @T2 
+SELECT
+B.PtNo_Num AS [ENCOUNTER ID]
+, B.ClasfCd AS [DX CD]
+, B.ClasfPrio [DX PRIORITY CD]
+
+-- WHERE IT COMES FROM
+FROM (
+	SELECT PtNo_Num
+	, ClasfCd
+	, ClasfPrio
+	FROM smsdss.BMH_PLM_PtAcct_Clasf_Dx_V
+	WHERE SortClasfType = 'DF'
+	AND ClasfPrio IN (
+		1,2,3
+	)
+	AND PtNo_Num < '20000000'
+) B
+/*
+-----------------------------------------------------------------------
+END OF QUERY TWO (2)
+-----------------------------------------------------------------------
+*/
+/*
+-----------------------------------------------------------------------
+PUTTING IT ALL TOGETHER NOW
+-----------------------------------------------------------------------
+*/
+SELECT *
+
+FROM @T1 T1
+LEFT JOIN @T2 T2
+ON T1.[ENCOUNTER ID] = T2.[ENCOUNTER ID]
