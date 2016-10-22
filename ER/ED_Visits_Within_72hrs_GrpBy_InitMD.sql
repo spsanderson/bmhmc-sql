1fbe96af8856869cde5e55ed36d3b09efdd19210
Steven Sanderson <spsanderson@gmail.com>
2016-10-22 12:39:20 -0400

    addition of press ganey query, addition of all copd dsrip file, updates to 72 hour ed returns and addition of HCRA file

--- ER/ED_Visits_Within_72hrs_GrpBy_InitMD.sql
+++ ER/ED_Visits_Within_72hrs_GrpBy_InitMD.sql
@@ -9,6 +9,7 @@ WITH CTE AS (
 	, Diagnosis
 	, ICD9
 	, Arrival as [Arrival Datetime]
+	, TimeLeftED
 	, ED_MD
 	, Disposition
 
@@ -29,7 +30,7 @@ SELECT C1.Account       AS [Initial Account]
 , C2.[Arrival Datetime] AS [Arrival 2]
 , c1.Disposition        AS [Initial Dispo]
 , c2.Disposition        AS [Secondary Dispo]
-, DATEDIFF(HOUR, c1.[Arrival Datetime], C2.[Arrival Datetime]) AS [Interim Hours]
+, DATEDIFF(HOUR, c1.TimeLeftED, C2.[Arrival Datetime]) AS [Interim Hours]
 
 FROM CTE       C1
 INNER JOIN CTE C2
@@ -37,15 +38,16 @@ ON C1.MR# = C2.MR#
 
 WHERE C1.[Arrival Datetime] <> C2.[Arrival Datetime]
 AND C1.RN+1 = C2.RN
-AND DATEDIFF(HOUR, c1.[Arrival Datetime], c2.[Arrival Datetime]) <= 72
+AND DATEDIFF(HOUR, c1.TimeLeftED, c2.[Arrival Datetime]) <= 72
+and LEFT(c1.TimeLeftED, 4) = '2016'
 
 -- Use below to get between specific dates
---AND c1.[Arrival Datetime] >= ''
---AND c1.[Arrival Datetime] < ''
+--AND c1.[Arrival Datetime] >= '2016-09-01'
+--AND c1.[Arrival Datetime] < '2016-10-01'
 
 -- Use below to get last Sunday - Saturday
-AND c1.[Arrival Datetime] >= DATEADD(DD, -1, DATEADD(WK, DATEDIFF(WK, 0, GETDATE()), -7))
-AND c1.[Arrival Datetime] < DATEADD(DD, -1, DATEADD(WK, DATEDIFF(WK, 0, GETDATE()), -0))
+--AND c1.[Arrival Datetime] >= DATEADD(DD, -1, DATEADD(WK, DATEDIFF(WK, 0, GETDATE()), -7))
+--AND c1.[Arrival Datetime] < DATEADD(DD, -1, DATEADD(WK, DATEDIFF(WK, 0, GETDATE()), -0))
 
 ORDER BY [Initial ED Md]
 , MR#
---
+++ Press Ganey/FINAL_DISCHARGE_ORDER.sql
@@ -0,0 +1,113 @@
+SELECT [SURVEY DESIGNATOR] = 'IN0101P'
+, [CLIENT ID] = '725'
+, [LAST NAME] = ISNULL(A.pt_last_name, '')
+, [MIDDLE INITIAL] = ''
+, [FIRST NAME] = ISNULL(A.pt_first_name , '')
+, [ADDRESS 1] = ISNULL(B.addr_line1, '')
+, [CITY] = ISNULL(B.addr_line2, '')
+, [STATE] = ISNULL(B.addr_line3, '')
+, [ZIP CODE] = ISNULL(PLM.Pt_Zip_Cd, '')
+, [TELEPHONE NUMBER] = ISNULL(A.pt_rpt_phone_no, '')
+, [MS-DRG] = ISNULL(PLM.drg_no, '')
+, [GENDER] = ISNULL(SEX.PT_SEX , '')
+, [DATE OF BIRTH] = REPLACE(CONVERT(VARCHAR(10), PLM.Pt_Birthdate, 101), '/','')
+, [LANGUAGE] = ISNULL(LANG.LANG, '')
+, [MEDICAL RECORD NUMBER] = PLM.Med_Rec_No
+, [UNIQUE ID] = PLM.PtNo_Num
+, [ADMISSION SOURCE] = ISNULL(ADM_SRC.ADM_SRC, '')
+, [ADMIT DATE] = REPLACE(CONVERT(VARCHAR(10), PLM.Adm_Date, 101), '/', '')
+, [DISCHARGE DATE] = REPLACE(CONVERT(VARCHAR(10), PLM.Dsch_Date, 101), '/', '')
+, [PATIENT DISCHARGE STATUS] = ISNULL(DSCH_DISP.DSCH_DISP, '')
+, [UNIT] = ISNULL(A.ward_cd, '')
+, [PATIENT EMAIL] = ISNULL(D.UserDataText, '')
+, [ATTENDING PHYSICIAN NPI] = ISNULL(E.npi_no, '')
+, [ATTENDING PHYSICIAN NAME] = ISNULL(E.pract_rpt_name, '')
+, [SPECIALTY] = ISNULL(E.med_staff_dept, '')
+, [ER_ADMIT] = ISNULL(PLM.ED_Adm, '')
+, [E.O.R INDICATOR] = '$'
+
+FROM smsdss.BMH_PLM_PtAcct_V             AS PLM
+INNER JOIN smsmir.vst_rpt                AS A
+ON PLM.Pt_No = A.pt_id
+LEFT JOIN smsmir.pers_addr               AS B
+ON A.pt_id = B.pt_id
+	AND B.pers_type = 'PT'
+LEFT MERGE JOIN smsdss.BMH_UserTwoFact_V AS C
+ON PLM.PtNo_Num = C.PtNo_Num
+	AND C.UserDataKey = '98'
+LEFT MERGE JOIN smsdss.BMH_UserTwoFact_V AS D
+ON PLM.PtNo_Num = D.PtNo_Num
+	AND D.UserDataKey = '631'
+LEFT MERGE JOIN smsdss.pract_dim_v       AS E
+ON PLM.Atn_Dr_No = E.src_pract_no
+	AND E.orgz_cd = 'S0X0'
+
+CROSS APPLY (
+	SELECT
+		CASE
+			WHEN PLM.Pt_Sex = 'M' THEN '1'
+			WHEN PLM.Pt_Sex = 'F' THEN '2'
+			ELSE 'M'
+	END AS PT_SEX
+) SEX
+
+CROSS APPLY (
+	SELECT
+		CASE
+			WHEN PLM.Adm_Source IN ('EO', 'RP', 'TA') THEN '1'
+			WHEN PLM.Adm_Source = 'TE' THEN '5'
+			WHEN PLM.Adm_Source = 'RM' THEN '3'
+			WHEN PLM.Adm_Source IN ('TH', 'TO') THEN '4'
+			WHEN PLM.Adm_Source = 'OP' THEN '2'
+			WHEN PLM.Adm_Source = 'TB' THEN '8'
+			WHEN PLM.Adm_Source = 'TV' THEN 'D'
+			WHEN PLM.Adm_Source = 'AS' THEN 'E'
+			WHEN PLM.Adm_Source = 'HS' THEN 'F'
+			ELSE 'M'
+	END AS ADM_SRC
+) ADM_SRC
+
+CROSS APPLY (
+	SELECT
+		CASE
+			WHEN PLM.dsch_disp IN ('AHR','AHF', 'HR', 'HF') THEN '01'
+			WHEN PLM.dsch_disp IN ('ATW', 'TW') THEN '06'
+			WHEN PLM.dsch_disp IN ('AMA', 'MA') THEN '07'
+			WHEN PLM.dsch_disp IN ('ATE', 'ATL', 'TE', 'TL') THEN '03'
+			WHEN PLM.dsch_disp IN ('ATH', 'TH') THEN '02'
+			WHEN PLM.dsch_disp IN ('ATV', 'ATO', 'AMN', 'ATF') THEN '05'
+			WHEN PLM.dsch_disp IN ('ATR', 'ATI') THEN '04'
+			WHEN PLM.dsch_disp IN ('ATT', 'AHI') THEN '50'
+			WHEN PLM.dsch_disp IN ('ATP', 'AOU', 'AHB') THEN '65'
+			WHEN PLM.dsch_disp IN ('ATM', 'ATX') THEN '62'
+			WHEN PLM.dsch_disp IN ('ATB') THEN '21'
+			WHEN PLM.dsch_disp IN ('ATN') THEN '43'
+			WHEN LEFT(PLM.dsch_disp, 1) IN ('C', 'D') THEN '20'
+			ELSE 'M'
+	END AS DSCH_DISP
+) DSCH_DISP
+
+CROSS APPLY (
+	SELECT
+		CASE
+			WHEN C.UserDataText = 'ENGLISH' THEN '0'
+			WHEN C.UserDataText = 'SPANISH' THEN '1'
+			WHEN C.UserDataText = 'RUSSIAN' THEN '3'
+			WHEN C.UserDataText = 'ITALIAN' THEN '5'
+			WHEN C.UserDataText = 'POLISH'  THEN '6'
+			WHEN C.UserDataText = 'CHINESE' THEN '10'
+			ELSE ''
+	END AS LANG
+) LANG
+
+--WHERE PLM.Dsch_Date >= '2016-03-01'
+--AND PLM.DSCH_DATE < '2016-04-01'
+WHERE PLM.DSCH_DATE = CAST(GETDATE()-1 AS DATE)
+AND PLM.Plm_Pt_Acct_Type = 'I'
+AND PLM.tot_chg_amt > 0
+AND LEFT(PLM.PTNO_NUM, 4) != '1999'
+AND PLM.PtNo_Num < '20000000'
+AND LEFT(PLM.dsch_disp, 1) NOT IN ('C', 'D')
+AND PLM.Adm_Source NOT IN ('NE', 'NB')
+
+OPTION(FORCE ORDER);
