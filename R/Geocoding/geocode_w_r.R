38e1b8f3b35387fc4407ed42d142201799649fc7
Steven Sanderson <spsanderson@gmail.com>
2019-02-11 21:19:56 -0500

    DSK geocode server has been deprecated, use geocode_OSM

--- R/Geocoding/geocode_w_r.R
+++ R/Geocoding/geocode_w_r.R
@@ -1,10 +1,9 @@
 # Geocoding a csv column of "addresses" in R
-
-#load ggmap
-library(ggmap)
+# Lib Load ####
 library(readxl)
+library(tmaptools)
 
-# Select the file from the file chooser
+# Get File ####
 #fileToLoad <- file.choose(new = TRUE)
 fileToLoad <- "S:\\Global Finance\\1 REVENUE CYCLE\\Steve Sanderson II\\ALOS_Readmit_Mapping\\daily_geocode_file.xlsx"
 
@@ -14,44 +13,86 @@ origAddress <- read_xlsx(fileToLoad, col_names = TRUE)
 # Initialize the data frame
 geocoded <- data.frame(stringsAsFactors = FALSE)
 
-# Loop through the addresses to get the latitude and longitude of each address and add it to the
-# origAddress data frame in new columns lat and lon
+# Geocode File ####
 for(i in 1:nrow(origAddress)) {
-  print(paste("Working on geocoding:", origAddress$FullAddress[i]))
-  # changed to dsk as google map api's keeps returning over_query_limit
-  result <- geocode(
-    origAddress$FullAddress[i]
-    , output = "latlon"
-    , source = "dsk"
-    )
-  origAddress$lon[i] <- as.numeric(result[1])
-  origAddress$lat[i] <- as.numeric(result[2])
-  #origAddress$geoAddress[i] <- as.character(result[3])
-  #Sys.sleep(1)
-}
-
-# since the map api sucks you may need to run the below a few times
-for(i in 1:nrow(origAddress)){
-  if(is.na(origAddress[i,'lon'])){
-    print(paste("Working on geocoding:"
-      , origAddress$FullAddress[i]
-      , "there are: "
-      , geocodeQueryCheck()
-      , " geocding queries left"
+  print(paste("Working on geocoding: ", origAddress$FullAddress[i]))
+  if(
+    is.null(
+      suppressWarnings(
+        suppressMessages(
+          geocode_OSM(
+            origAddress$FullAddress[i]
+            )
+          )
+        )
       )
+    ) {
+    print(
+      paste(
+        "Coult not get record for: "
+        , origAddress$FullAddress[i]
+        , ". Trying next record..."
+        )
     )
-    result <- geocode(
-      origAddress$FullAddress[i]
-      , output = "latlon"
-      , source = "dsk"
-      )
-    origAddress$lon[i] <- as.numeric(result[1])
-    origAddress$lat[i] <- as.numeric(result[2])
-    #origAddress$geoAddress <- as.character(result[3])
+    origAddress$lon[i] <- ''
+    origAddress$lat[i] <- ''
   } else {
-    print("Trying next record...")
+    print(
+      paste(
+        "Getting Result For: "
+        , origAddress$FullAddress[i]
+      )
+    )
+    result <- geocode_OSM(
+      origAddress$FullAddress[i]
+      , return.first.only = T
+      , as.data.frame = T
+    )
+    origAddress$lon[i] <- as.numeric(result[3])
+    origAddress$lat[i] <- as.numeric(result[2])
   }
 }
+# Loop through the addresses to get the latitude and longitude of each address and add it to the
+# origAddress data frame in new columns lat and lon
+# Google requires an API key and it is paid
+# for(i in 1:nrow(origAddress)) {
+#   print(paste("Working on geocoding:", origAddress$FullAddress[i]))
+#   result <- geocode(
+#     origAddress$FullAddress[i]
+#     , output = "latlon"
+#     , source = "google"
+#     )
+#   origAddress$lon[i] <- as.numeric(result[1])
+#   origAddress$lat[i] <- as.numeric(result[2])
+# }
+
+# since the map api sucks you may need to run the below a few times
+# for(i in 1:nrow(origAddress)){
+#   if(is.na(origAddress[i,'lon'])){
+#     print(paste("Working on geocoding:"
+#       , origAddress$FullAddress[i]
+#       , "there are: "
+#       , geocodeQueryCheck()
+#       , " geocding queries left"
+#       )
+#     )
+#     result <- geocode(
+#       origAddress$FullAddress[i]
+#       , output = "latlon"
+#       , source = "dsk"
+#       )
+#     origAddress$lon[i] <- as.numeric(result[1])
+#     origAddress$lat[i] <- as.numeric(result[2])
+#     #origAddress$geoAddress <- as.character(result[3])
+#   } else {
+#     print("Trying next record...")
+#   }
+# }
 
 # Write a CSV file containing origAddress to the working directory
-write.csv(origAddress, "geocoded_addresses.csv", row.names=FALSE)
+# Clean up origAddress file if lat and/or lon are missing
+geocoded <- origAddress %>%
+  dplyr::filter(
+    origAddress$lat != "" | origAddress$lon != ""
+  )
+write.csv(geocoded, "geocoded_addresses.csv", row.names=FALSE)
