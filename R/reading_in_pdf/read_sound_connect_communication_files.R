74195ae7b7c4b4d6d84078d3eea9422faa15730a
Steven Sanderson <spsanderson@gmail.com>
2019-03-24 14:58:15 -0400

    add try and tryCatch logic

--- R/reading_in_pdf/read_sound_connect_communication_files.R
+++ R/reading_in_pdf/read_sound_connect_communication_files.R
@@ -7,35 +7,72 @@ library(data.table)
 file.vector <- list.files(path = getwd())
 file.vector %>% head()
 
-# is file a pdf
+# Is PDF? ####
 grepl(".pdf", file.vector)
 
-# get list of pdfs
+# Get PDF List ####
 pdf.list <- file.vector[grepl(".pdf", file.vector)]
 print(pdf.list)
 
-cur.doc <- extract_tables(pdf.list[1])[[1]]
-print(cur.doc)
-
+# Create DF ####
 documents <- data.frame()
+error.page.df <- data.frame()
 
+# For Loop ####
 for(i in 1:length(pdf.list)){
-  print(paste("Reading - ", pdf.list[i]))
+  print(paste("Reading file -", pdf.list[i]))
   cur.doc <- extract_tables(pdf.list[i])
-  for(j in 1:length(cur.doc[i])){
-    cur.doc.page <- cur.doc[[j]]
+  print(paste("There are", length(cur.doc), "pages in the current file."))
+  for(j in 1:length(cur.doc)){
+    cur.doc.page <- cur.doc[j]
+    print(
+      paste(
+        "Reading page -"
+        , j
+        , "There are"
+        , ncol(as.data.frame(cur.doc.page))
+        , "columns."
+        )
+      )
     df <- as.data.frame(cur.doc.page)
+    df <- df[-1, ]
+    df <- df[, colSums(df != "") != 0]
     df$FileName <- pdf.list[i]
-    documents <- rbind(documents, df)
+    tmp.col.names <- c(
+      "V1","V2","V3","V4","V6","FileName"
+    )
+    try(colnames(df) <- tmp.col.names, silent = T)
+    possible.error <- try(rbind(documents, df))
+    if(isTRUE(class(possible.error)=="try-error")) { 
+      print(
+        paste(
+          "Could not insert page"
+          , j
+          , "for file -"
+          , pdf.list[i]
+        )
+      )
+      error.msg <- paste(
+        "Could not insert page"
+        , j
+        , "for file -"
+        , pdf.list[i]
+      )
+      error.page.df <- rbind(error.page.df, error.msg)
+      next 
+    } else {
+      documents <-rbind(documents, df)
+      possible.error <- NA
+    }
   }
 }
 
+# Clean DF ####
 column.names <- c(
   "Patient"
   , "MRN"
   , "Room_Number"
   , "Physician"
-  , "Pysician_Pager"
   , "Team"
   , "FileName"
 )
@@ -48,8 +85,12 @@ documents <- filter(
 ) %>%
   filter(
     !(Patient %like% "Confidentiality Notice")
-)
+) %>%
+  filter(
+    MRN != "" | Room_Number != "" | Physician != "" | Team != "" 
+  )
 
 documents <- as_tibble(documents)
 
+# Write file ####
 write.csv(documents, "Sound_Connect_Communication.csv")
