c7c7177da20ada4c4f1ca0c2796148d7a35e2f84
Steven Sanderson <spsanderson@gmail.com>
2019-04-26 15:20:32 -0400

    add totals by floor by qtr

--- R/geographic_rounding_2NS.R
+++ R/geographic_rounding_2NS.R
@@ -23,7 +23,7 @@ freq(
   )
 )
 
-# Viz ####
+# Viz Grouped ####
 # Data and AES
 readmit.plt <- df %>%
   group_by(DSCH_YYYYqN, ward_cd, HOSPITALIST_Pvt_CD_FLAG) %>%
@@ -221,6 +221,215 @@ avg.soi.plt <- df %>%
     , x = ""
     , y = ""
     , fill = "Hosp / Pvt"
+  ) +
+  theme(
+    legend.background = element_blank()
+  ) +
+  theme(
+    legend.key = element_blank()
+  ) +
+  tidyquant::theme_tq()
+# Print Graph
+print(avg.soi.plt)
+
+# Viz Not Grouped ####
+# Data and AES
+readmit.plt <- df %>%
+  group_by(DSCH_YYYYqN, ward_cd) %>%
+  summarise(
+    Ra_Rate = round(sum(RA_FLAG) / sum(Enc_Flag), 4) * 100
+  ) %>%
+  ggplot(
+    aes(
+      x = as.factor(DSCH_YYYYqN)
+      , y = Ra_Rate
+      , fill = DSCH_YYYYqN
+    )
+  ) +
+  # Geometries
+  geom_bar(
+    stat = "identity"
+    , color = "black"
+    , position = "dodge"
+    , alpha = 0.5
+  ) +
+  # Facets
+  facet_grid(. ~ ward_cd) +
+  # Statistics
+  # Coordinates
+  geom_text(
+    aes(
+      label = Ra_Rate
+    )
+    , vjust = 1.5
+    , color = "black"
+    , position = position_dodge(0.9)
+    , size = 3
+  ) +
+  # Theme
+  labs(
+    title = "Readmit Rate by Discharge Year and Quarter"
+    , subtitle = "Patients discharged from 2SOU & 2NOR"
+    , x = ""
+    , y = ""
+    , fill = "YYYYqN"
+  ) +
+  theme(
+    legend.background = element_blank()
+  ) +
+  theme(
+    legend.key = element_blank()
+  ) +
+  tidyquant::theme_tq()
+# Print Graph
+print(readmit.plt)
+
+# Data and AES
+# Patient had to have telemetr
+appropriate.tele.plt <- df %>%
+  filter(TELE_FLAG == 1) %>%
+  group_by(DSCH_YYYYqN, ward_cd) %>%
+  summarise(
+    App.Tele.Rate = round(sum(Tele_Adm_Dx_Ind) / sum(TELE_FLAG), 4) * 100
+  ) %>%
+  ggplot(
+    aes(
+      x = as.factor(DSCH_YYYYqN)
+      , y = App.Tele.Rate
+      , fill = DSCH_YYYYqN
+    )
+  ) +
+  # Geometries
+  geom_bar(
+    stat = "identity"
+    , color = "black"
+    , position = "dodge"
+    , alpha = 0.5
+  ) +
+  # Facets
+  facet_grid(. ~ ward_cd) +
+  # Statistics
+  # Coordinates
+  geom_text(
+    aes(
+      label = App.Tele.Rate
+    )
+    , vjust = 1.5
+    , color = "black"
+    , position = position_dodge(0.9)
+    , size = 3
+  ) +
+  # Theme
+  labs(
+    title = "Telemetry Indication Rate by Discharge Year and Quarter"
+    , subtitle = "Patients discharged from 2SOU & 2NOR"
+    , x = ""
+    , y = ""
+    , fill = "YYYYqN"
+    , caption = "Percentage of patients who had an admitting dx for telemetry"
+  ) +
+  theme(
+    legend.background = element_blank()
+  ) +
+  theme(
+    legend.key = element_blank()
+  ) +
+  tidyquant::theme_tq()
+# Print Graph
+print(appropriate.tele.plt)
+
+# Data and AES
+excess.days.plt <- df %>%
+  group_by(DSCH_YYYYqN, ward_cd) %>%
+  summarise(
+    excess.days.stay = round(sum(excess.days, na.rm = T), 0)
+  ) %>%
+  ggplot(
+    aes(
+      x = as.factor(DSCH_YYYYqN)
+      , y = excess.days.stay
+      , fill = DSCH_YYYYqN
+    )
+  ) +
+  # Geometries
+  geom_bar(
+    stat = "identity"
+    , color = "black"
+    , position = "dodge"
+    , alpha = 0.5
+  ) +
+  # Facets
+  facet_grid(. ~ ward_cd) +
+  # Statistics
+  # Coordinates
+  geom_text(
+    aes(
+      label = excess.days.stay
+    )
+    , vjust = 1.5
+    , color = "black"
+    , position = position_dodge(0.9)
+    , size = 3
+  ) +
+  # Theme
+  labs(
+    title = "Total Excess Days by Discharge Year and Quarter"
+    , subtitle = "Patients discharged from 2SOU & 2NOR"
+    , x = ""
+    , y = ""
+    , fill = "YYYYqN"
+    , caption = "Excess Days: Actual LOS - Exp LOS (Smaller Number is Better)"
+  ) +
+  theme(
+    legend.background = element_blank()
+  ) +
+  theme(
+    legend.key = element_blank()
+  ) +
+  tidyquant::theme_tq()
+# Print Graph
+print(excess.days.plt)
+
+# Data and AES
+avg.soi.plt <- df %>%
+  group_by(DSCH_YYYYqN, ward_cd) %>%
+  summarise(
+    avg.soi = round(mean(Index_SOI, na.rm = T), 2)
+  ) %>%
+  ggplot(
+    aes(
+      x = as.factor(DSCH_YYYYqN)
+      , y = avg.soi
+      , fill = DSCH_YYYYqN
+    )
+  ) +
+  # Geometries
+  geom_bar(
+    stat = "identity"
+    , color = "black"
+    , position = "dodge"
+    , alpha = 0.5
+  ) +
+  # Facets
+  facet_grid(. ~ ward_cd) +
+  # Statistics
+  # Coordinates
+  geom_text(
+    aes(
+      label = avg.soi
+    )
+    , vjust = 1.5
+    , color = "black"
+    , position = position_dodge(0.9)
+    , size = 3
+  ) +
+  # Theme
+  labs(
+    title = "Mean SOI (Severity Of Illness) by Discharge Year and Quarter"
+    , subtitle = "Patients discharged from 2SOU & 2NOR"
+    , x = ""
+    , y = ""
+    , fill = "YYYYqN"
   ) +
   theme(
     legend.background = element_blank()
