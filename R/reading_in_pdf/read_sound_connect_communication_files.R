86918fffd235214a39c7973d27ae0e0008d82b80
Steven Sanderson <spsanderson@gmail.com>
2019-06-08 14:17:52 -0400

    minor change

--- R/reading_in_pdf/read_sound_connect_communication_files.R
+++ R/reading_in_pdf/read_sound_connect_communication_files.R
@@ -1,8 +1,10 @@
 # Lib Load ####
-library(tidyverse)
-library(tabulizer)
-library(data.table)
-library(shiny)
+install.load::install_load(
+  "tidyverse"
+  , "tabulizer"
+  , "data.table"
+  , "shiny"
+)
 
 # Doc List ####
 file.vector <- list.files(path = getwd())
@@ -101,10 +103,10 @@ all.documents <- data.frame(stringsAsFactors = FALSE)
 all.documents <- documents
 
 f <- tryCatch(file.choose(new = T), error = function(e) "")
-f.data <- extract_areas(f, 2)
+f.data <- extract_areas(f, 3)
 f.data.df <- as.data.frame(f.data, stringsAsFactors = FALSE)
 f.data.df$X5 <- NA
-f.data.df$FileName = 'SoundConnectCommunication_2019_05_14_04_30.pdf'
+f.data.df$FileName = 'SoundConnectCommunication_2019_06_07_04_30.pdf'
 #View(f.data.df)
 # is mrn column blank
 f.data.df$MRN <- str_sub(f.data.df$X1, -6, -1)
@@ -140,4 +142,6 @@ all.documents <- bind_rows(all.documents, f.data.df)
 
 # Write file ####
 write.csv(documents, "Sound_Connect_Communication.csv")
+rm(list = ls())
 write.csv(all.documents, "Sound_Connect_Communication.csv")
+rm(list = ls())
