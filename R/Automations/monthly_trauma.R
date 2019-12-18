3178c393289740c3334223bb6f79d0761c428eec
Steven Paul Sanderson II, MPH <spsanderson@users.noreply.github.com>
2019-12-17 21:49:39 -0500

    delete email

--- R/Automations/monthly_trauma.R
+++ R/Automations/monthly_trauma.R
@@ -428,7 +428,7 @@ Outlook <- COMCreate("Outlook.Application")
 Email = Outlook$CreateItem(0)
 
 # Set the recipeitn, subject, and body
-Email[["to"]] = "EHuang@LICommunityHospital.org"
+Email[["to"]] = "email-placeholder"
 Email[["cc"]] = ""
 Email[["bcc"]] = ""
 Email[["subject"]] = "Monthly Trauma Files"
