31423c35a860fc2ebcfe0404595d4b82f11423f9
Steven Sanderson <spsanderson@gmail.com>
2020-06-28 15:58:18 -0400

    date update

--- R/reading_in_pdf/read_sound_connect_communication_files.R
+++ R/reading_in_pdf/read_sound_connect_communication_files.R
@@ -120,10 +120,10 @@ all.documents <- data.frame(stringsAsFactors = FALSE)
 all.documents <- documents
 
 f <- tryCatch(file.choose(new = T), error = function(e) "")
-f.data <- extract_areas(f, 3)
+f.data <- extract_areas(f, 2)
 f.data.df <- as.data.frame(f.data, stringsAsFactors = FALSE)
 f.data.df$X5 <- NA
-f.data.df$FileName = 'SoundConnectCommunication_2020_05_12_04_30.pdf'
+f.data.df$FileName = 'SoundConnectCommunication_2020_06_26_04_30.pdf'
 #View(f.data.df)
 # is mrn column blank
 f.data.df$MRN <- str_sub(f.data.df$X1, -6, -1)
@@ -133,10 +133,10 @@ f.data.df <- f.data.df %>%
     , MRN
     #, Room
     #, Provider
-    #, X2
+    , X2
     , X3
     , X4
-    , X5
+    #, X5
     , FileName
   )
 
