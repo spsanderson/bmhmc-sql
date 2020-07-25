94d1b45162772b02dc3ccf1a12fa9df3eb53aec5
Steven Sanderson <spsanderson@gmail.com>
2020-07-24 21:27:38 -0400

    minor change

--- R/avg_ed_census_arrivals.R
+++ R/avg_ed_census_arrivals.R
@@ -168,7 +168,7 @@ df_cen_bucket <- df.census.gathered %>%
   mutate(hour_bucket = hour_bucket %>% as_factor())
 
 # Visualize ####
-capt <- "From 12-29-2019 to 07-4♦-2020"
+capt <- "From 12-29-2019 to 07-18-2020"
 
 arrivals.boxplt <- df.arrivals.gathered %>% ggplot(
   aes(
