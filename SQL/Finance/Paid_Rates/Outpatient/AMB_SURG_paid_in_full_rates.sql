ca0d26b196a5cfadf2623856214324a0051f1db0
Steven Sanderson <spsanderson@gmail.com>
2020-10-30 15:16:06 -0400

    avg paid rates queries

---
+++ SQL/Finance/Paid_Rates/Outpatient/AMB_SURG_paid_in_full_rates.sql
@@ -0,0 +1,111 @@
+/*
+***********************************************************************
+File: AMB_SURG_paid_in_full_rates.sql
+
+Input Parameters:
+	None
+
+Tables/Views:
+	smsdss.vst_fct_v (updates 1st 8th and every saturday)
+    smsdss.pt_type_dim_v
+    smsdss.pyr_dim_v
+
+Creates Table:
+	Enter Here
+
+Functions:
+	Enter Here
+
+Author: Steven P Sanderson II, MPH
+
+Department: Finance, Revenue Cycle
+
+Purpose/Description
+	Gets counts by FC and total payments for all pt types paid in full
+
+Revision History:
+Date		Version		Description
+----		----		----
+2020-10-06	v1			Initial Creation
+***********************************************************************
+*/
+
+-- DATE VARIABLES FOR ALL ACCOUNTS
+DECLARE @START DATE
+DECLARE @END DATE
+
+-- Go back 9 months
+SET @START = DATEADD(MONTH, DATEDIFF(MONTH, 0, CAST(GETDATE() AS DATE)) - 9, 0)
+-- First day of current month
+SET @END = DATEADD(MONTH, DATEDIFF(MONTH, 0, CAST(GETDATE() AS DATE)), 0)
+
+-- PT TYPE @ TABLE
+DECLARE @PTTYPE TABLE (
+	PT_TYPE_CD_DESC VARCHAR(255),
+	ORGZ_CD VARCHAR(255),
+	PT_TYPE VARCHAR(255)
+	)
+
+INSERT INTO @PTTYPE
+SELECT DISTINCT pt_type_cd_desc,
+	orgz_cd,
+	pt_type
+FROM SMSPHDSSS0X0.smsdss.pt_type_dim_v
+WHERE orgz_cd = 'S0X0'
+ORDER BY pt_type_cd_desc
+
+-- PYR GROUP @ TABLE
+DECLARE @PYR_GROUP TABLE (
+	PYR_GROUP_DESC VARCHAR(255),
+	PYR_GROUP VARCHAR(255),
+	PYR_CD VARCHAR(255),
+	ORGZ_CD VARCHAR(255)
+	)
+
+INSERT INTO @PYR_GROUP
+SELECT DISTINCT pyr_group2,
+	pyr_group,
+	pyr_cd,
+	orgz_cd
+FROM SMSDSS.pyr_dim_v
+WHERE orgz_cd = 'S0X0'
+	AND pyr_group2 NOT IN ('?', 'No Description')
+ORDER BY pyr_group;
+
+-- CROSS JOIN TBL
+SELECT DISTINCT PT.PT_TYPE,
+	PT.PT_TYPE_CD_DESC,
+	PG.PYR_CD,
+	PG.PYR_GROUP,
+	PG.PYR_GROUP_DESC,
+	PG.ORGZ_CD
+INTO #PT_PG
+FROM @PTTYPE AS PT
+CROSS JOIN @PYR_GROUP AS PG
+
+-- DATA
+SELECT PTPG.pyr_group,
+	PTPG.PYR_GROUP_DESC,
+	COUNT(DISTINCT VST.PT_ID) AS [VISITS],
+	ISNULL(SUM(VST.TOT_CHG_AMT), 0) AS [TOTAL_CHARGES],
+	ISNULL(SUM(VST.RPT_TOT_PAY_AMT), 0) AS [TOTAL_PAYMENT_AMT],
+	ISNULL(SUM(VST.TOT_BAL_AMT), 0) AS [TOTAL_AMOUNT_DUE],
+	ISNULL(SUM(VST.RPT_TOT_ADJ_AMT), 0) AS [TOTAL_ALLOWANCE]
+FROM #PT_PG AS PTPG
+LEFT OUTER JOIN SMSDSS.VST_FCT_V AS VST ON PTPG.PT_TYPE = VST.pt_type_2
+	AND PTPG.PYR_CD = VST.prim_pyr_cd
+	AND PTPG.ORGZ_CD = VST.orgz_cd
+	AND VST.vst_end_date >= @START
+	AND VST.vst_end_date < @END
+	AND VST.pt_type_2 NOT IN ('C', 'E', 'K', 'R', 'V')
+	AND VST.vst_type_cd = 'O'
+	AND VST.tot_chg_amt > 0
+    AND VST.TOT_BAL_AMT >= - 50
+    AND VST.TOT_BAL_AMT <= 50
+WHERE PTPG.PT_TYPE IN ('D','G')
+	AND VST.prim_pyr_cd NOT IN ('B75','B76')
+GROUP BY PTPG.pyr_group,
+	PTPG.PYR_GROUP_DESC
+ORDER BY PTPG.pyr_group
+
+DROP TABLE #PT_PG
---
+++ SQL/Finance/Paid_Rates/Outpatient/HBOT_Paid_Unpaid.sql
@@ -0,0 +1,87 @@
+/*
+***********************************************************************
+File: HBOT_Paid_Unpaid.sql
+
+Input Parameters:
+	None
+
+Tables/Views:
+	smsdss.BMH_PLM_PtAcct_V
+    smsmir.actv
+
+Creates Table:
+	Enter Here
+
+Functions:
+	Enter Here
+
+Author: Steven P Sanderson II, MPH
+
+Department: Finance, Revenue Cycle
+
+Purpose/Description
+	Gets counts for all HBOT accounts and total payments by FC paid
+	in full and not.
+
+Revision History:
+Date		Version		Description
+----		----		----
+2020-10-06	v1			Initial Creation
+***********************************************************************
+*/
+
+DECLARE @START DATE
+DECLARE @END DATE
+
+-- First day of current year
+SET @START = DATEADD(YEAR, DATEDIFF(YEAR, 0, CAST(GETDATE() AS DATE)), 0)
+-- First day of current month
+SET @END = DATEADD(MONTH, DATEDIFF(MONTH, 0, CAST(GETDATE() AS DATE)), 0)
+
+DECLARE @PYR_GROUP TABLE (
+	PYR_GROUP_DESC VARCHAR(255),
+	PYR_GROUP VARCHAR(255)
+	)
+
+INSERT INTO @PYR_GROUP
+SELECT DISTINCT pyr_group2,
+	pyr_group
+FROM SMSDSS.pyr_dim_v
+WHERE orgz_cd = 'S0X0'
+	AND pyr_group2 NOT IN ('?', 'No Description');
+
+SELECT b.user_pyr1_cat,
+	COUNT(b.pt_no) AS 'VISITS',
+	SUM(b.tot_chg_amt) AS 'Total_Charges',
+	SUM(b.tot_pay_amt) AS 'Total_Payment_Amt',
+	SUM(b.tot_amt_due) AS 'Total_Amount_Due',
+	SUM(b.tot_adj_amt) AS 'Total_Allowance'
+INTO #RECORDS
+FROM smsdss.BMH_PLM_PtAcct_V AS b
+WHERE b.pt_no IN (
+		SELECT DISTINCT (a.pt_id)
+		FROM smsmir.actv AS a
+		WHERE a.actv_cd IN ('02501005', '02501013', '02501021', '02501039', '02501047', '02501054', '02501062', '02501070', '02501088', '02501096')
+			AND a.actv_date >= @START
+			AND A.actv_date < @END
+			AND a.hosp_svc = 'WCC'
+			AND a.chg_tot_amt <> 0
+		)
+	AND B.Pyr1_Co_Plan_Cd NOT IN ('B75', 'B76')
+GROUP BY b.user_pyr1_cat
+HAVING SUM(b.tot_chg_amt) > 0
+ORDER BY user_pyr1_cat;
+
+SELECT PG.PYR_GROUP,
+	ISNULL(REC.VISITS, 0) AS [VISITS],
+	ISNULL(REC.TOTAL_CHARGES, 0) AS [TOTAL_CHARGES],
+	ISNULL(REC.TOTAL_PAYMENT_AMT, 0) * - 1 AS [TOTAL_PAYMENT_AMT],
+	ISNULL(REC.TOTAL_AMOUNT_DUE, 0) AS [TOTAL_AMOUNT_DUE],
+	ISNULL(REC.TOTAL_ALLOWANCE, 0) AS [TOTAL_ALLOWANCE]
+FROM @PYR_GROUP AS PG
+LEFT OUTER JOIN #RECORDS AS REC ON PG.PYR_GROUP = REC.user_pyr1_cat
+
+DROP TABLE #RECORDS
+
+
+
