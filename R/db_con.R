5c730464a308456a6345807240bd2e10c1625aef
Steven Paul Sanderson II, MPH <spsanderson@users.noreply.github.com>
2021-04-02 15:04:50 -0400

    Update db_con.R

--- R/db_con.R
+++ R/db_con.R
@@ -2,8 +2,8 @@
 db_con <- DBI::dbConnect(
   odbc::odbc(),
   Driver = "SQL Server",
-  #Server = "LI-HIDB",
-  Server = "BMH-HIDB",
+  Server = "LI-HIDB",
+  #Server = "BMH-HIDB",
   Database = "SMSPHDSSS0X0",
   Trusted_Connection = T
 )
