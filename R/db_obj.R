30f505d70beb25a8cba94c84640855783831666a
Steven Paul Sanderson II, MPH <spsanderson@users.noreply.github.com>
2021-04-02 15:04:52 -0400

    Update db_obj.R

--- R/db_obj.R
+++ R/db_obj.R
@@ -25,8 +25,8 @@ db_connect <- function() {
   db_con <- DBI::dbConnect(
     odbc::odbc(),
     Driver = "SQL Server",
-    #Server = "LI-HIDB",
-    Server = "BMH-HIDB",
+    Server = "LI-HIDB",
+    #Server = "BMH-HIDB",
     Database = "SMSPHDSSS0X0",
     Trusted_Connection = TRUE
   )
