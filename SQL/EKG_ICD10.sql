0a9ec5c925550a960d6d7b7e74acaf200d32839c
Steven Paul Sanderson II, MPH <spsanderson@users.noreply.github.com>
2021-04-02 15:05:26 -0400

    Update EKG_ICD10.sql

--- SQL/EKG_ICD10.sql
+++ SQL/EKG_ICD10.sql
@@ -4,22 +4,51 @@ GO
 DECLARE @D DATETIME;
 DECLARE @SD DATE;
 DECLARE @ED DATE;
-SET @D = GETDATE();
-SET @SD = GETDATE() - 7;
-SET @ED = GETDATE() - 1;
-----------
+DECLARE @OD DATE;
 
-SELECT A.episode_no AS [PatientNumber]
-, a.fc
-, LEFT(B.vst_type_cd, 1) AS [Accomodation]
-, '' AS [Co-payment_Amount]
-, '' AS [Co-payment Paid]
-, a.pty_cd AS [Referring_Dr_No]
-, UPPER(REVERSE(PARSENAME(REPLACE(REVERSE(A.PTY_NAME), ',', '.'), 1))) AS PTY_NAME
-, CONVERT(VARCHAR(8), A.Order_Stop_Date, 112) AS [Date_Of_Service]
-, '' AS [Second_FC]
-, a.ord_no AS [Client_Trans_Ref_No]
-, CASE
+SET @D = GETDATE();
+SET @SD = DATEADD(DAY, DATEDIFF(DAY, 0, @D) - 7, 0);
+SET @ED = DATEADD(DAY, DATEDIFF(DAY, 0, @D) - 1, 0);
+SET @OD = DATEADD(DAY, DATEDIFF(DAY, 0, @D) - (365*2), 0);
+
+SELECT A.episode_no,
+A.FC,
+LEFT(B.VST_TYPE_CD, 1) AS [Accomodation],
+'' AS [Co-payment_Amount],
+'' AS [Co-payment Paid],
+A.pty_cd AS [Referring_Dr_No],
+UPPER(REVERSE(PARSENAME(REPLACE(REVERSE(A.PTY_NAME), ',', '.'),1))) AS [pty_name],
+CONVERT(VARCHAR(8), A.Order_Stop_Date, 112) AS [Date_Of_Service],
+'' AS [Second_FC],
+A.ord_no AS [Client_Trans_Ref_No],
+A.ord_sts,
+A.ord_no,
+A.ord_occr_no,
+D.Date_Coded
+INTO #TEMPA
+FROM SMSDSS.c_sr_orders_finance_rpt_v AS A
+LEFT OUTER JOIN SMSDSS.c_bmh_coder_activity_v AS D
+ON A.episode_no = D.episode_no
+LEFT OUTER JOIN SMSMIR.sr_vst_pms AS B ON A.episode_no = B.episode_no
+	AND A.pt_id_start_dtime = B.pt_id_start_dtime
+
+WHERE A.svc_cd IN ('00424994','00600015')
+AND D.Date_Coded BETWEEN @SD AND @ED;
+
+SELECT B.episode_no AS [PatientNumber],
+b.fc,
+b.Accomodation,
+b.[Co-payment_Amount],
+b.[Co-payment Paid],
+b.Referring_Dr_No,
+b.pty_name,
+b.Date_Of_Service,
+b.Second_FC,
+b.Client_Trans_Ref_No,
+b.ord_sts,
+b.ord_no,
+B.Date_Coded,
+CASE
        -- WHEN THERE IS A THRID SPACE AND IT IS BEFORE THE COMMA
        WHEN CHARINDEX(',', f.readingdr) > 0 -- COMMA LOC
        AND CHARINDEX(' ', f.readingdr, CHARINDEX(' ', f.readingdr, CHARINDEX(' ', f.readingdr) + 1) + 1) < CHARINDEX(',', f.readingdr) -- THIRD SPACE LOC < COMMA LOC
@@ -38,39 +67,20 @@ SELECT A.episode_no AS [PatientNumber]
               ))
        
               ELSE ''
-  END AS [Reading_Dr_Last_Name]
-, CASE
+  END AS [Reading_Dr_Last_Name],
+CASE
        WHEN charindex(' ', f.readingdr) != 0 
               THEN UPPER(LEFT(f.readingdr, (charindex(' ', f.readingdr) -1)))
        ELSE ''
-  END as [Reading_Dr_First_Name]
-, D.Date_Coded
-, a.ord_sts
-, a.ord_no
-, F.ReadingDr
-
-INTO #TEMP_ORD
-
-FROM smsdss.c_sr_orders_finance_rpt_v as a 
-LEFT JOIN smsmir.sr_vst_pms as b
-ON a.episode_no = b.episode_no 
-       AND a.pt_id_start_dtime = b.pt_id_start_dtime
-LEFT OUTER JOIN smsmir.obsv AS C
-ON a.episode_no = C.episode_no
-       AND a.ord_occr_no = C.ord_occr_no
-LEFT OUTER JOIN smsdss.c_bmh_coder_activity_v AS D
-ON a.episode_no = D.episode_no
-LEFT OUTER JOIN smsmir.mir_sc_InvestigationResultSuppInfo AS F
-ON C.rslt_supl_info_obj_id = F.ObjectID
-
-WHERE a.svc_cd IN ('00424994','00600015')
-AND a.Order_Start_Date >= '2017-01-01'
-AND C.obsv_cd_name IN ('EKG', 'EKG-OP','EKG RESULTS')
-AND C.val_sts_cd = 'F'
-AND D.Date_Coded BETWEEN @SD AND @ED
-
-OPTION(FORCE ORDER)
-;
+  END as [Reading_Dr_First_Name],
+F.ReadingDr
+INTO #TEMPB
+FROM SMSMIR.obsv AS A
+INNER JOIN #TEMPA AS B ON A.episode_no = B.episode_no
+	AND A.ord_occr_no = B.ord_occr_no
+	AND A.val_sts_cd = 'F'
+	--AND A.obsv_cd_name IN ('EKG', 'EKG-OP','EKG RESULTS') -- SUPERFLUOUS ALREADY JOINS ON ORD_OCC_NO
+LEFT OUTER JOIN SMSMIR.mir_sc_InvestigationResultSuppInfo AS F ON A.rslt_supl_info_obj_id = F.ObjectID;
 
 SELECT A.PatientNumber
 , A.fc
@@ -92,9 +102,8 @@ SELECT A.PatientNumber
 , A.ord_sts
 , A.ord_no
 , A.ReadingDr
-INTO #TEMP_ORD_B
-FROM #TEMP_ORD AS A
-;
+INTO #TEMPC
+FROM #TEMPB AS A;
 
 SELECT A.PatientNumber
 , A.fc
@@ -104,10 +113,10 @@ SELECT A.PatientNumber
 , A.Referring_Dr_No
 , A.FIRST_NAME AS REF_DR_FIRST_NAME
 , CASE
-	WHEN A.LAST_NAME_B IS NOT NULL
-		THEN CONCAT(A.LAST_NAME_A, ' ', A.LAST_NAME_B)
-	WHEN A.LAST_NAME_A IS NULL
-		THEN A.MIDDLE_NAME
+       WHEN A.LAST_NAME_B IS NOT NULL
+              THEN CONCAT(A.LAST_NAME_A, ' ', A.LAST_NAME_B)
+       WHEN A.LAST_NAME_A IS NULL
+              THEN A.MIDDLE_NAME
     ELSE A.LAST_NAME_A
     END AS [Ref_Dr_Last_Name]
 , A.Date_Of_Service
@@ -119,12 +128,8 @@ SELECT A.PatientNumber
 , A.ord_sts
 , A.ord_no
 , A.ReadingDr
-INTO #TEMP_ORD_C
-FROM #TEMP_ORD_B AS A
-GO
-;
-
-----------
+INTO #TEMPD
+FROM #TEMPC AS A;
 
 SELECT PtNo_Num AS [PatientNumber]
 , LTRIM(RTRIM(Med_Rec_No)) AS Med_Rec_No
@@ -150,59 +155,43 @@ SELECT PtNo_Num AS [PatientNumber]
 INTO #TEMP_DX
 
 FROM (
-       SELECT DX.PtNo_Num
+	SELECT PLM.PtNo_Num
        , PLM.Med_Rec_No
        , PLM.Pt_Name
        , PDV.src_pract_no
        , PDV.pract_rpt_name
-       , DX.ClasfCd
-       , DX.ClasfPrio
-       
-       FROM smsdss.BMH_PLM_PtAcct_Clasf_Dx_V AS DX
-       LEFT OUTER JOIN smsdss.BMH_PLM_PtAcct_V AS PLM
-       ON DX.PT_NO = PLM.Pt_No
-              AND DX.Pt_Key = PLM.Pt_Key
-              AND DX.Bl_Unit_Key = PLM.Bl_Unit_Key
-       LEFT OUTER JOIN smsdss.pract_dim_v AS PDV
-       ON PLM.Atn_Dr_No = PDV.src_pract_no
-              AND PLM.Regn_Hosp = PDV.orgz_cd
-       
-       WHERE DX.ClasfSch = '0'
-       AND DX.SortClasfType = 'DF'
-       --AND DX.Pt_No = ''
+       , DX.dx_cd
+       , DX.dx_cd_prio
+	FROM SMSDSS.BMH_PLM_PtAcct_V AS PLM
+	LEFT OUTER JOIN SMSMIR.dx_grp AS DX ON PLM.Pt_No = DX.pt_id
+		AND PLM.unit_seq_no = DX.unit_seq_no
+		AND PLM.from_file_ind = DX.from_file_ind
+	LEFT OUTER JOIN SMSDSS.PRACT_DIM_V AS PDV ON PLM.Atn_Dr_No = PDV.src_pract_no
+		AND PLM.Regn_Hosp = PDV.orgz_cd
+	INNER JOIN #TEMPD AS D ON PLM.PtNo_Num = D.PatientNumber
+	WHERE DX.dx_cd_schm = '0'
+	AND LEFT(DX.dx_cd_type, 2) = 'DF'
+	AND DX.dx_cd_prio < '11'
 ) AS A
 
 PIVOT(
-       MAX(CLASFCD)
-       FOR CLASFPRIO IN (
+       MAX(DX_CD)
+       FOR DX_CD_PRIO IN (
               "01","02","03","04","05","06","07","08","09","10"
        )
-) AS PVT
+) AS PVT;
 
-GO
-;
-
-----------
--- ADMITTING DX
-SELECT PtNo_Num
-, ClasfCd
-
+SELECT DISTINCT SUBSTRING(D.PT_ID, 5, 8) AS [PtNo_Num],
+D.dx_cd
 INTO #TEMP_ADM_DX
-
-FROM smsdss.BMH_PLM_PtAcct_Clasf_Dx_V AS D
-WHERE D.SortClasfType = 'DA'
-AND D.ClasfSch = '0'
-AND D.ClasfPrio IN ('01', '1')
-AND D.PtNo_Num IN (
-       SELECT ZZZ.PatientNumber
-       FROM #TEMP_ORD AS ZZZ
-)
-
-GO
+FROM SMSMIR.dx_grp AS D
+INNER JOIN #TEMPA AS A ON SUBSTRING(D.pt_id, 5, 8) = A.episode_no
+WHERE LEFT(D.dx_cd_type, 2) = 'DA'
+AND D.dx_cd_schm = '0'
+AND D.dx_cd_prio IN ('01','1')
 ;
 
-----------
-
+-- FINAL SELECT
 SELECT A.PatientNumber
 , B.Med_Rec_No
 , B.PT_LastName
@@ -223,7 +212,7 @@ SELECT A.PatientNumber
 , A.Ref_Dr_First_Name
 , A.Second_FC
 , A.Client_Trans_Ref_No
-, C.ClasfCd AS [Admit_Dx]
+, C.dx_cd AS [Admit_Dx]
 , UPPER(A.Reading_Dr_Last_Name) AS [Reading_Dr_Last_Name]
 , A.Reading_Dr_First_Name
 , '' as [Dx3]
@@ -254,27 +243,17 @@ SELECT A.PatientNumber
 , '' as [Dx28]
 , '' as [Dx29]
 , '' as [Dx30]
---, A.Date_Coded
+, A.Date_Coded
 --, A.ord_sts
 --, A.ord_no
 --, A.ReadingDr
 
-FROM #TEMP_ORD_C AS A
+FROM #TEMPD AS A
 LEFT OUTER JOIN #TEMP_DX AS B
 ON A.PatientNumber = B.PatientNumber
 LEFT OUTER JOIN #TEMP_ADM_DX AS C
 ON A.PatientNumber = C.PtNo_Num
-ORDER BY A.PatientNumber
+ORDER BY A.PatientNumber;
 
-GO
-;
-
-----------
-
-DROP TABLE #TEMP_ADM_DX
-DROP TABLE #TEMP_DX
-DROP TABLE #TEMP_ORD
-DROP TABLE #TEMP_ORD_B
-DROP TABLE #TEMP_ORD_C
-
-GO
+-- DROP TEMP TABLES
+DROP TABLE #TEMPA,#TEMPB,#TEMPC,#TEMPD,#TEMP_DX,#TEMP_ADM_DX;
