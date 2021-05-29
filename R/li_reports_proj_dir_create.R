024193f174de2e91ff9c2bda9af34c7d5da6c44f
Steven Paul Sanderson II, MPH <spsanderson@users.noreply.github.com>
2021-05-29 15:21:20 -0400

    Update li_reports_proj_dir_create.R

--- R/li_reports_proj_dir_create.R
+++ R/li_reports_proj_dir_create.R
@@ -16,3 +16,60 @@ folders <- c(
 fs::dir_create(
   path = folders
 )
+
+
+file_create("01_Queries/query_functions.R")
+file_create("02_Data_Manipulation/data_functions.R")
+file_create("03_Viz/viz_functions.R")
+file_create("04_TS_Modeling/ts_functions.R")
+
+# DSS Connection 
+db_connect <- function() {
+  db_con <- DBI::dbConnect(
+    odbc::odbc(),
+    Driver             = "SQL Server",
+    Server             = "LI-HIDB",
+    Database           = "SMSPHDSSS0X0",
+    Trusted_Connection = TRUE
+  )
+  
+  return(db_con)
+  
+}
+
+# Disconnect from Database
+db_disconnect <- function(.connection) {
+  
+  DBI::dbDisconnect(
+    conn = db_connect()
+  )
+  
+}
+
+# Library Load
+
+library_load <- function(){
+  
+  if(!require(pacman)){install.packages("pacman")}
+  pacman::p_load(
+    "DBI"
+    , "odbc"
+    , "janitor"
+    , "dplyr"
+    , "tibble"
+    , "tidyr"
+  )
+  
+}
+
+db_funs <- c("db_connect","db_disconnect")
+dump(
+  list = db_funs,
+  file = "00_Scripts/db_con_obj.R"
+)
+
+lib_funs <- "library_load"
+dump(
+  list = lib_funs,
+  file = "00_Scripts/library_load.R"
+)
