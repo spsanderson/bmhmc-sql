26312c573132de7786fc11238b7784d468ee35e9
Steven Paul Sanderson II, MPH <spsanderson@users.noreply.github.com>
2021-05-29 15:21:26 -0400

    Update csv_files_to_dss.R

--- R/nysdoh_sepsis/csv_files_to_dss.R
+++ R/nysdoh_sepsis/csv_files_to_dss.R
@@ -3,15 +3,21 @@ library(DBI)
 library(odbc)
 library(dbplyr)
 
-file_list <- dir("G:/IS/C Wurtz/Sepsis/csv_files21/Treatment/"
+folder <- "Treatment"
+path   <- "G:/IS/C Wurtz/Sepsis/csv_files211/"
+full_path <- paste0(path,folder,"/")
+
+file_list <- dir(full_path
                  , pattern = "\\.csv$"
                  , full.names = T)
+
 files <- file_list %>%
   map(read.csv)
 
 file_names <- file_list %>%
-  str_remove("G:/IS/C Wurtz/Sepsis/csv_files21/Treatment/") %>%
-  str_replace(pattern = "_VerD2.1.csv", replacement = "_v21.csv")
+  #str_remove("G:/IS/C Wurtz/Sepsis/csv_files21/Treatment/") %>%
+  str_remove(full_path) %>%
+  str_replace(pattern = "_VerD2.1.1.csv", replacement = "_v211.csv")
 
 names(files) <- file_names
 
@@ -32,7 +38,7 @@ for(i in 1:length(files)){
 con <- LICHospitalR::db_connect()
 for(i in 1:length(files)){
   file_to_dss_name <- paste0("c_nysdoh_sepsis_", names(files[i]))
-  file_name_clean  <- file_to_dss_name %>% str_replace("_v21.csv","")
+  file_name_clean  <- file_to_dss_name %>% str_replace("_v211.csv","")
   file_tbl         <- files[[i]]
   print(paste0("Inserting ",file_name_clean, " into DSS"))
   DBI::dbWriteTable(
