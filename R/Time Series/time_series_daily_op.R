b10906fd4798860802000c28eae12392467373fd
Steven Paul Sanderson II, MPH <spsanderson@users.noreply.github.com>
2021-07-25 09:00:58 -0400

    Update time_series_daily_op.R

--- R/Time Series/time_series_daily_op.R
+++ R/Time Series/time_series_daily_op.R
@@ -5,16 +5,22 @@ if(!require(pacman)) install.packages("pacman")
 pacman::p_load(
   "tidymodels",
   "modeltime",
-  "tidyverse",
+  "dplyr",
   "lubridate",
   "timetk",
   "odbc",
   "DBI",
   "janitor",
-  "timetk",
   "tidyquant",
-  "anomalize"
+  "modeltime.ensemble",
+  "modeltime.resample",
+  "stringr",
+  "workflowsets",
+  "parallel",
+  "sknifedatar"
 )
+
+n_cores = detectCores() - 1
 
 interactive <- TRUE
 
@@ -24,13 +30,7 @@ map(paste0(my_path, file_list), source)
 
 # DB Connection -----------------------------------------------------------
 
-db_con <- dbConnect(
-  odbc(),
-  Driver = "SQL Server",
-  Server = "BMH-HIDB",
-  Database = "SMSPHDSSS0X0",
-  Trusted_Connection = T
-)
+db_con <- LICHospitalR::db_connect()
 
 # Query -------------------------------------------------------------------
 
@@ -72,22 +72,22 @@ dbDisconnect(db_con)
 
 # Manipulate --------------------------------------------------------------
 
-query <- query %>%
+data_tbl <- query %>%
   summarise_by_time(
     .date_var = date_col
     , .by = "month"
-    , visit_count = sum(dsch_count)
+    , value = sum(dsch_count)
   )
 
 # TS Plot -----------------------------------------------------------------
 
-start_date <- min(query$date_col)
-end_date   <- max(query$date_col)
+start_date <- min(data_tbl$date_col)
+end_date   <- max(data_tbl$date_col)
 
 plot_time_series(
-  .data = query
+  .data = data_tbl
   , .date_var = date_col
-  , .value = visit_count
+  , .value = value
   , .title = paste0(
     "Monthly OP Registrations from: "
     , start_date
@@ -98,146 +98,302 @@ plot_time_series(
 )
 
 plot_seasonal_diagnostics(
-  .data = query
+  .data = data_tbl
   , .date_var = date_col
-  , .value = visit_count
+  , .value = value
 )
 
 plot_anomaly_diagnostics(
-  .data = query
+  .data = data_tbl
   , .date_var = date_col
-  , .value = visit_count
+  , .value = value
 )
 
 
 # Data Split --------------------------------------------------------------
 
-splits <- initial_time_split(query, prop = 0.9)
+splits <- initial_time_split(data_tbl, prop = 0.8)
 
-# Models ----
+# Features ----------------------------------------------------------------
+
+recipe_base <- recipe(value ~ ., data = training(splits))
+
+recipe_date <- recipe_base %>%
+  step_timeseries_signature(date_col) %>%
+  step_rm(matches("(iso$)|(xts$)|(hour)|(min)|(sec)|(am.pm)")) %>%
+  step_normalize(contains("index.num"), contains("date_col_year"))
+
+recipe_fourier <- recipe_date %>%
+  step_dummy(all_nominal_predictors(), one_hot = TRUE) %>%
+  step_fourier(date_col, period = 365/52, K = 1) %>%
+  step_YeoJohnson(value, limits = c(0,1))
+
+recipe_fourier_final <- recipe_fourier %>%
+  step_nzv(all_predictors())
+
+recipe_pca <- recipe_base %>%
+  step_timeseries_signature(date_col) %>%
+  step_rm(matches("(iso$)|(xts$)|(hour)|(min)|(sec)|(am.pm)")) %>%
+  step_dummy(all_nominal_predictors(), one_hot = TRUE) %>%
+  step_normalize(value) %>%
+  step_fourier(date_col, period = 365/52, K = 1) %>%
+  step_normalize(all_numeric_predictors()) %>%
+  step_nzv(all_predictors()) %>%
+  step_pca(all_numeric_predictors(), threshold = .95)
+
+# Models ------------------------------------------------------------------
 
 # Auto ARIMA --------------------------------------------------------------
 
-model_fit_arima_no_boost <- arima_reg() %>%
-  set_engine(engine = "auto_arima") %>%
-  fit(visit_count ~ date_col, data = training(splits))
-
+model_spec_arima_no_boost <- arima_reg() %>%
+  set_engine(engine = "auto_arima")
 
 # Boosted Auto ARIMA ------------------------------------------------------
 
-model_fit_arima_boosted <- arima_boost(
+model_spec_arima_boosted <- arima_boost(
   min_n = 2
   , learn_rate = 0.015
 ) %>%
-  set_engine(engine = "auto_arima_xgboost") %>%
-  fit(
-    visit_count ~ date_col 
-    + as.numeric(date_col) 
-    + factor(month(date_col, label = TRUE), ordered = FALSE)
-    , data = training(splits)
-  )
+  set_engine(engine = "auto_arima_xgboost")
 
 # ETS ---------------------------------------------------------------------
 
-model_fit_ets <- exp_smoothing() %>%
-  set_engine(engine = "ets") %>%
-  fit(visit_count ~ date_col, data = training(splits))
+model_spec_ets <- exp_smoothing(
+  seasonal_period = "auto",
+  error = "auto",
+  trend = "auto",
+  season = "auto",
+  damping = "auto"
+) %>%
+  set_engine(engine = "ets") 
+
+model_spec_croston <- exp_smoothing(
+  seasonal_period = "auto",
+  error = "auto",
+  trend = "auto",
+  season = "auto",
+  damping = "auto"
+) %>%
+  set_engine(engine = "croston")
+
+model_spec_theta <- exp_smoothing(
+  seasonal_period = "auto",
+  error = "auto",
+  trend = "auto",
+  season = "auto",
+  damping = "auto"
+) %>%
+  set_engine(engine = "theta")
+
+
+# STLM ETS ----------------------------------------------------------------
+
+model_spec_stlm_ets <- seasonal_reg(
+  seasonal_period_1 = "auto",
+  seasonal_period_2 = "auto",
+  seasonal_period_3 = "auto"
+) %>%
+  set_engine("stlm_ets")
+
+model_spec_stlm_tbats <- seasonal_reg(
+  seasonal_period_1 = "auto",
+  seasonal_period_2 = "auto",
+  seasonal_period_3 = "auto"
+) %>%
+  set_engine("tbats")
+
+model_spec_stlm_arima <- seasonal_reg(
+  seasonal_period_1 = "auto",
+  seasonal_period_2 = "auto",
+  seasonal_period_3 = "auto"
+) %>%
+  set_engine("stlm_arima")
+
+# NNETAR ------------------------------------------------------------------
+
+model_spec_nnetar <- nnetar_reg(
+  seasonal_period = "auto"
+  , penalty = 0.5
+  , epochs = 12
+) %>%
+  set_engine("nnetar")
+
 
 # Prophet -----------------------------------------------------------------
 
-model_fit_prophet <- prophet_reg() %>%
-  set_engine(engine = "prophet") %>%
-  fit(visit_count ~ date_col, data = training(splits))
+model_spec_prophet <- prophet_reg(
+  seasonality_yearly = "auto",
+  seasonality_weekly = "auto",
+  seasonality_daily = "auto"
+) %>%
+  set_engine(engine = "prophet")
 
-model_fit_prophet_boost <- prophet_boost(learn_rate = 0.1) %>% 
-  set_engine("prophet_xgboost") %>%
-  fit(
-    visit_count ~ date_col + 
-      as.numeric(date_col) + 
-      factor(month(date_col), label = TRUE)
-    , data = training(splits)
-  )
-
+model_spec_prophet_boost <- prophet_boost(
+  learn_rate = 0.1
+  , trees = 10
+  , seasonality_yearly = "auto"
+  , seasonality_weekly = "auto"
+  , seasonality_daily = "auto"
+) %>% 
+  set_engine("prophet_xgboost") 
 
 # TSLM --------------------------------------------------------------------
 
-model_fit_lm <- linear_reg() %>%
-  set_engine("lm") %>%
-  fit(
-    visit_count ~ as.numeric(date_col) 
-    + factor(month(date_col, label = TRUE), ordered = FALSE)
-    , data = training(splits)
-  )
+model_spec_lm <- linear_reg() %>%
+  set_engine("lm")
+
+model_spec_glm <- linear_reg(
+  penalty = 1,
+  mixture = 0.5
+) %>%
+  set_engine("glmnet")
+
+model_spec_stan <- linear_reg() %>%
+  set_engine("stan")
+
+model_spec_spark <- linear_reg(
+  penalty = 1,
+  mixture = 0.5
+) %>% 
+  set_engine("spark")
+
+model_spec_keras <- linear_reg(
+  penalty = 1,
+  mixture = 0.5
+) %>%
+  set_engine("keras")
 
 # MARS --------------------------------------------------------------------
 
 model_spec_mars <- mars(mode = "regression") %>%
   set_engine("earth")
 
-recipe_spec <- recipe(visit_count ~ date_col, data = training(splits)) %>%
-  step_date(date_col, features = "month", ordinal = FALSE) %>%
-  step_mutate(date_num = as.numeric(date_col)) %>%
-  step_normalize(date_num) %>%
-  step_rm(date_col)
+# Workflowsets ------------------------------------------------------------
 
-wflw_fit_mars <- workflow() %>%
-  add_recipe(recipe_spec) %>%
-  add_model(model_spec_mars) %>%
-  fit(training(splits))
+wfsets <- workflow_set(
+  preproc = list(
+    base          = recipe_base,
+    date          = recipe_date,
+    fourier       = recipe_fourier,
+    fourier_final = recipe_fourier_final
+  ),
+  models = list(
+    model_spec_arima_no_boost,
+    model_spec_arima_boosted,
+    model_spec_ets,
+    model_spec_lm,
+    model_spec_glm,
+    # model_spec_stan,
+    # model_spec_spark,
+    # model_spec_keras,
+    model_spec_mars,
+    model_spec_nnetar,
+    model_spec_prophet,
+    model_spec_prophet_boost,
+    model_spec_stlm_arima,
+    model_spec_stlm_ets,
+    model_spec_stlm_tbats
+  ),
+  cross = TRUE
+)
+
+parallel_start(n_cores)
+wf_fits <- wfsets %>% 
+  modeltime_fit_workflowset(
+    data = data_tbl
+    , control = control_fit_workflowset(
+      allow_par = TRUE
+      , verbose = TRUE
+    )
+  )
+parallel_stop()
+
+wf_fits <- wf_fits %>%
+  filter(.model_desc != "NULL")
 
 # Model Table -------------------------------------------------------------
 
-models_tbl <- modeltime_table(
-  model_fit_arima_no_boost,
-  model_fit_arima_boosted,
-  model_fit_ets,
-  model_fit_prophet,
-  model_fit_prophet_boost,
-  model_fit_lm, 
-  wflw_fit_mars
-)
+models_tbl <- wf_fits
+
+# Model Ensemble Table ----------------------------------------------------
+
+fit_mean_ensemble <- models_tbl %>%
+  ensemble_average(type = "mean")
+
+fit_median_ensemble <- models_tbl %>%
+  ensemble_average(type = "median")
+
+# Model Table -------------------------------------------------------------
+
+models_tbl <- models_tbl %>%
+  add_modeltime_model(fit_mean_ensemble) %>%
+  add_modeltime_model(fit_median_ensemble)
 
 models_tbl
 
 # Calibrate Model Testing -------------------------------------------------
+parallel_start(n_cores)
 
 calibration_tbl <- models_tbl %>%
+  #modeltime_refit(training(splits)) %>%
   modeltime_calibrate(new_data = testing(splits))
+
+parallel_stop()
+
 calibration_tbl
 
 # Testing Accuracy --------------------------------------------------------
 
+parallel_start(n_cores)
+
 calibration_tbl %>%
   modeltime_forecast(
     new_data = testing(splits),
-    actual_data = query
+    actual_data = data_tbl
   ) %>%
   plot_modeltime_forecast(
     .legend_max_width = 25,
     .interactive = interactive
   )
+parallel_stop()
 
 calibration_tbl %>%
   modeltime_accuracy() %>%
-  arrange(mae) %>%
-  table_modeltime_accuracy(resizable = TRUE, bordered = TRUE)
+  arrange(desc(rsq)) %>%
+  table_modeltime_accuracy(.interactive = FALSE)
 
 # Refit to all Data -------------------------------------------------------
-
+parallel_start(n_cores)
 refit_tbl <- calibration_tbl %>%
-  modeltime_refit(data = query)
+  modeltime_refit(
+    data = data_tbl
+    , control = control_refit(
+      verbose   = TRUE
+    )
+  )
+parallel_stop()
 
 top_two_models <- refit_tbl %>% 
   modeltime_accuracy() %>% 
   arrange(mae) %>% 
   slice(1:2)
 
+ensemble_models <- refit_tbl %>%
+  filter(
+    .model_desc %>%
+      str_to_lower() %>%
+      str_detect("ensemble")
+  ) %>%
+  modeltime_accuracy()
+
+model_choices <- rbind(top_two_models, ensemble_models)
+
 refit_tbl %>%
   filter(.model_id %in% top_two_models$.model_id) %>%
-  modeltime_forecast(h = "1 year", actual_data = query) %>%
+  modeltime_forecast(h = "1 year", actual_data = data_tbl) %>%
   plot_modeltime_forecast(
-    .legend_max_width = 25
-    , .interactive = interactive
+    .interactive = FALSE
+    , .conf_interval_show = FALSE
     , .title = "Monthly OP Regsitrations Forecast 1 Year Out"
   )
 
@@ -264,9 +420,9 @@ calibration_tbl %>%
   theme_tq()
 
 ts_sum_arrivals_plt(
-  .data = query
+  .data = data_tbl
   , .date_col = date_col
-  , .value_col = visit_count
+  , .value_col = value
   , .x_axis = mn
   , .ggplt_group_var = yr
   , yr
@@ -280,9 +436,9 @@ ts_sum_arrivals_plt(
   )
 
 ts_median_excess_plt(
-  .data = query
+  .data = data_tbl
   , .date_col = date_col
-  , .value_col = visit_count
+  , .value_col = value
   , .x_axis = mn
   , .ggplt_group_var = yr
   , .secondary_grp_var = mn
