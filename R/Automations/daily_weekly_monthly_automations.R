f7d20565250ccb76274e88d8358015bcfa356298
Steven Paul Sanderson II, MPH <spsanderson@users.noreply.github.com>
2021-08-09 09:11:28 -0400

    Update daily_weekly_monthly_automations.R

--- R/Automations/daily_weekly_monthly_automations.R
+++ R/Automations/daily_weekly_monthly_automations.R
@@ -1,8 +1,12 @@
+# Lib Load ----
 library(RDCOMClient)
 library(LICHospitalR)
 
+# Daily ----
+# * Wound Care ----
 source("S:/Global Finance/1 REVENUE CYCLE/Steve Sanderson II/Code/R/Automations/wound_care_daily_batch.R")
 
+# * Respiratory Email ----
 respiratory_vae_query() %>%
   respiratory_vae_tbl() %>%
   respiratory_vae_automation(
@@ -10,92 +14,115 @@ respiratory_vae_query() %>%
     , .email = ""
   )
 
+# * Respiratory File Save ----
 respiratory_vae_query() %>%
   respiratory_vae_tbl() %>%
   save_to_excel(.file_name = "VAE")
 
+# * Geocode ----
 geocode_discharges_automation()
 
+# Weekly ----
+# * Weekly PSY Discharges ----
 weekly_psy_discharges_query() %>%
   weekly_psy_discharges_automation(
     .delete_file = TRUE
     , .email = ""
   )
 
+# * Code 64 ----
 code64_automation(
   .delete_file = TRUE
-  , .email = 
+  , .email = c("")
 )
 
+# * Duplicate Cataracts ----
 duplicate_coded_cataracts_automation(
   .delete_file = TRUE
-  , .email = 
+  , .email = ""
 )
 
+# * * ORSOS ----
+# * ORSOS to SPROC ----
 orsos_to_sproc_query() %>%
   orsos_to_sproc_tbl() %>%
   orsos_to_sproc_automation(
     .delete_file = TRUE
-    , .email = )
+    , .email = c("")
   )
 
+# * ORSOS J Accounts ----
 orsos_j_accounts_query() %>%
   orsos_j_accounts_automation(
     .delete_file = TRUE
-    , .email = 
+    , .email = ""
   )
 
+# * Experian Return to DSS ----
+source("S:/Global Finance/1 REVENUE CYCLE/Steve Sanderson II/Code/R/Automations/exp_return_file_to_dss.R")
+
+
+# Monthly ----
+# * Congenital Malformation ----
 congenital_malformation_automation(
   .delete_file = TRUE
-  , .email = 
+  , .email = ""
 )
 
+# * PSY Admits and Discharges ----
 monthly_psy_admits_discharges_tbl() %>%
   monthly_psy_admits_discharges_automation(
     .delete_file = TRUE
-    , .email = 
+    , .email = c("")
   )
 
+# * TRAUMA ----
 monthly_trauma_tbl() %>%
   monthly_trauma_automation(
     .delete_file = TRUE
-    , .email = 
+    , .email = ""
   )
 
+# * MyHealth Sx ----
 myhealth_monthly_surgery_query() %>%
   myhealth_monthly_surgery_tbl() %>%
   myhealth_monthly_surgery_automation(
     .delete_file = TRUE
-    , .email = 
+    , .email = c("")
   )
 
+# * Discharge Order to Discharge ----
 discharge_order_to_discharge_automation(
   .delete_file = TRUE
-  , .email = 
+  , .email = c("")
 )
 
+# * IP Coding Lag ----
 inpatient_coding_lag_query() %>%
   inpatient_coding_lag_tbl() %>%
   inpatient_coding_lag_automation(
     .delete_file = TRUE
-    , .email = 
+    , .email = ""
   )
 
+# * Infection Prevention ----
 infection_prevention_patient_days_query() %>%
   infection_prevention_patient_days_tbl() %>%
   infection_prevention_patient_days_automation(
     .delete_file = TRUE
-      , .email = 
+      , .email = ""
   )
 
+# * CDI QEC ----
 qec_cdi_automation(
   .delete_file = TRUE
-  , .email = 
+  , .email = ""
 )
 
+# * Readmits PSY to PSY ----
 readmit_psy_to_psy_query() %>%
   readmit_psy_to_psy_tbl() %>%
   readmit_psy_to_psy_automation(
     .delete_file = TRUE
-    , .email = 
+    , .email = ""
   )
