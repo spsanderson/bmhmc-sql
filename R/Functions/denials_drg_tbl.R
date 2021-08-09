786de4566321cb2b084dfed67ac6bd87091a2d2f
Steven Paul Sanderson II, MPH <spsanderson@users.noreply.github.com>
2021-08-09 09:12:20 -0400

    Update denials_drg_tbl.R

--- R/Functions/denials_drg_tbl.R
+++ R/Functions/denials_drg_tbl.R
@@ -4,8 +4,8 @@ denials_drg_tbl <- function() {
         filter(adm_yr >= max(adm_yr) - 4) %>%
         filter(payer_category == 'Other') %>%
         filter(patient_type == "I") %>%
-        select(record_flag, drg, adm_yr) %>%
-        group_by(adm_yr, drg) %>%
+        select(record_flag, drg, drg_name, adm_yr) %>%
+        group_by(adm_yr, drg, drg_name) %>%
         summarise(denial_count = sum(record_flag, na.rm = TRUE)) %>%
         ungroup() %>%
         group_by(adm_yr) %>%
@@ -13,9 +13,9 @@ denials_drg_tbl <- function() {
         mutate(record_flag = row_number()) %>%
         ungroup() %>%
         filter(record_flag <= 10) %>%
-        select(adm_yr, drg, denial_count) %>%
+        select(adm_yr, drg, drg_name, denial_count) %>%
         arrange(desc(adm_yr),desc(denial_count)) %>%
-        set_names("Admit_Year", "DRG", "Denial Count") %>%
+        set_names("Admit_Year", "DRG", "DRG Description", "Denial Count") %>%
         nest(drg_data = -Admit_Year)
     
     return(data)
