651218077013895695396553fcdb1575029f4853
Steven Paul Sanderson II, MPH <spsanderson@users.noreply.github.com>
2021-08-09 09:11:47 -0400

    Update geocode_w_r_automatic_working_file.R

--- R/Automations/geocode_w_r_automatic_working_file.R
+++ R/Automations/geocode_w_r_automatic_working_file.R
@@ -208,6 +208,9 @@ origAddress <- origAddress %>%
     , str_detect(PartialAddress, "MASTCIC NEACHJ")        ~ "MASTIC BEACH, NY, 11951"
     , str_detect(PartialAddress, "BELLLPORT")             ~ "BELLPORT, NY, 11713"
     , str_detect(PartialAddress, "NESCONSETT")            ~ "NESCONSET, NY, 11767"
+    , str_detect(PartialAddress, "YAHPANK")               ~ "YAPHANK, NY, 11980"
+    , str_detect(PartialAddress, "ISLIP TERRANCE")        ~ "ISLIP TERRACE, NY, 11752"
+    , str_detect(PartialAddress, "PORT SAT LUCY, FL")     ~ "PORT SAINT LUCIE, FL, 34952"
     , TRUE ~ PartialAddress
   ))
 
