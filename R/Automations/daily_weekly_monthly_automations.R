43ad2c363f8183e74856e041472940005eb2b00d
Steven Paul Sanderson II, MPH <spsanderson@users.noreply.github.com>
2021-10-26 13:18:56 -0400

    Update daily_weekly_monthly_automations.R

--- R/Automations/daily_weekly_monthly_automations.R
+++ R/Automations/daily_weekly_monthly_automations.R
@@ -11,7 +11,7 @@ respiratory_vae_query() %>%
   respiratory_vae_tbl() %>%
   respiratory_vae_automation(
     .delete_file = TRUE
-    , .email = "ELennon@licommunityhospital.org"
+    , .email = ""
   )
 
 # * Respiratory File Save ----
@@ -27,19 +27,19 @@ geocode_discharges_automation()
 weekly_psy_discharges_query() %>%
   weekly_psy_discharges_automation(
     .delete_file = TRUE
-    , .email = "EMullally@LICommunityHospital.org"
+    , .email = ""
   )
 
 # * Code 64 ----
 code64_automation(
   .delete_file = TRUE
-  , .email = c("EMullally@LICommunityHospital.org;DBabich@LICommunityHospital.org")
+  , .email = c("")
 )
 
 # * Duplicate Cataracts ----
 duplicate_coded_cataracts_automation(
   .delete_file = TRUE
-  , .email = "LProsper@licommunityhospital.org"
+  , .email = ""
 )
 
 # ORSOS ----
@@ -48,14 +48,14 @@ orsos_to_sproc_query() %>%
   orsos_to_sproc_tbl() %>%
   orsos_to_sproc_automation(
     .delete_file = TRUE
-    , .email = c("LProsper@licommunityhospital.org;MAki@LICommunityHospital.org")
+    , .email = c("")
   )
 
 # * ORSOS J Accounts ----
 orsos_j_accounts_query() %>%
   orsos_j_accounts_automation(
     .delete_file = TRUE
-    , .email = "CAKeenan@LICommunityHospital.org"
+    , .email = ""
   )
 
 # * Experian Return to DSS ----
@@ -65,21 +65,21 @@ source("S:/Global Finance/1 REVENUE CYCLE/Steve Sanderson II/Code/R/Automations/
 # * Congenital Malformation ----
 congenital_malformation_automation(
   .delete_file = TRUE
-  , .email = "LProsper@LICommunityHospital.org"
+  , .email = ""
 )
 
 # * PSY Admits and Discharges ----
 monthly_psy_admits_discharges_tbl() %>%
   monthly_psy_admits_discharges_automation(
     .delete_file = TRUE
-    , .email = c("KShaughness@LICommunityHospital.org;ESaporito@LICommunityHospital.org")
+    , .email = c("")
   )
 
 # * TRAUMA ----
 monthly_trauma_tbl() %>%
   monthly_trauma_automation(
     .delete_file = TRUE
-    , .email = "EHuang@LICommunityHospital.org"
+    , .email = ""
   )
 
 # * MyHealth Sx ----
@@ -87,13 +87,13 @@ myhealth_monthly_surgery_query() %>%
   myhealth_monthly_surgery_tbl() %>%
   myhealth_monthly_surgery_automation(
     .delete_file = TRUE
-    , .email = c("JPiscitelli@LICommunityHospital.org;WBayer@LICommunityHospital.org")
+    , .email = c("")
   )
 
 # * Discharge Order to Discharge ----
 discharge_order_to_discharge_automation(
   .delete_file = TRUE
-  , .email = c("JBaranowski-Guido@LICommunityHospital.org;MPontecorvo@LICommunityHospital.org")
+  , .email = c("")
 )
 
 # * IP Coding Lag ----
@@ -101,7 +101,7 @@ inpatient_coding_lag_query() %>%
   inpatient_coding_lag_tbl() %>%
   inpatient_coding_lag_automation(
     .delete_file = TRUE
-    , .email = "LProsper@LICommunityHospital.org"
+    , .email = ""
   )
 
 # * Infection Prevention ----
@@ -109,13 +109,13 @@ infection_prevention_patient_days_query() %>%
   infection_prevention_patient_days_tbl() %>%
   infection_prevention_patient_days_automation(
     .delete_file = TRUE
-      , .email = "DVirgil@LICommunityHospital.org"
+      , .email = ""
   )
 
 # * CDI QEC ----
 qec_cdi_automation(
   .delete_file = TRUE
-  , .email = "PMcKenna@LICommunityHospital.org"
+  , .email = ""
 )
 
 # * Readmits PSY to PSY ----
@@ -123,5 +123,5 @@ readmit_psy_to_psy_query() %>%
   readmit_psy_to_psy_tbl() %>%
   readmit_psy_to_psy_automation(
     .delete_file = TRUE
-    , .email = "ESaporito@LICommunityHospital.org"
+    , .email = ""
   )
