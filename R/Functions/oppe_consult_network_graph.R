79797e48119b1407b7a691680d59d7cd45370e8a
Steven Paul Sanderson II, MPH <spsanderson@users.noreply.github.com>
2021-12-06 09:13:45 -0500

    Update oppe_consult_network_graph.R

--- R/Functions/oppe_consult_network_graph.R
+++ R/Functions/oppe_consult_network_graph.R
@@ -23,7 +23,7 @@ oppe_consult_network_graph <- function(provider_id
     db_con <- dbConnect(
         odbc(),
         Driver = "SQL Server",
-        Server = "BMH-HIDB",
+        Server = "LI-HIDB",
         Database = "SMSPHDSSS0X0",
         Trusted_Connection = T
     )
