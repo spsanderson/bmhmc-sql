1853508a422da095f976a8e85395fd4b6253607c
Steven Paul Sanderson II, MPH <spsanderson@users.noreply.github.com>
2021-12-06 09:13:59 -0500

    Update oppe_provider_tbl.R

--- R/Functions/oppe_provider_tbl.R
+++ R/Functions/oppe_provider_tbl.R
@@ -4,7 +4,7 @@ oppe_provider_tbl <- function(provider_id) {
     db_con <- dbConnect(
         odbc(),
         Driver = "SQL Server",
-        Server = "BMH-HIDB",
+        Server = "LI-HIDB",
         Database = "SMSPHDSSS0X0",
         Trusted_Connection = T
     )
