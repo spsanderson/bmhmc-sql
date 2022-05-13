1a619dabcd1ea8b44f0da541797a5047b1dd70e4
Steven Paul Sanderson II, MPH <spsanderson@users.noreply.github.com>
2022-05-13 14:49:30 -0400

    Update csv_files_to_dss.R

--- R/nysdoh_sepsis/csv_files_to_dss.R
+++ R/nysdoh_sepsis/csv_files_to_dss.R
@@ -1,46 +1,44 @@
+# Library Load ----
 library(tidyverse)
 library(DBI)
 library(odbc)
 library(dbplyr)
+library(janitor)
+library(stringr)
 
+# Set file path ----
 folder <- "Treatment"
 path   <- "G:/IS/C Wurtz/Sepsis/csv_files30/"
 full_path <- paste0(path,folder,"/")
 
+# File List ----
 file_list <- dir(full_path
                  , pattern = "\\.csv$"
                  , full.names = T)
 
+# Read Files ----
 files <- file_list %>%
   map(read.csv) %>%
   map(as_tibble)
-  
 
+# Clean File Names ----
 file_names <- file_list %>%
   str_remove(full_path) %>%
   str_replace(pattern = "_VerD3.0.csv", replacement = "_v30.csv")
 
 names(files) <- file_names
 
-column_names <- c(
-  "pcs_code"
-  ,"pcs_code_description"
-  ,"subcategory"
+# Clean column names ----
+files <- map(files, clean_names)
+
+# Columns to Characters ----
+files <- map(
+  files,
+  ~ .x %>%
+    mutate(across(everything(), ~ str_squish(.x)))
 )
 
-for(i in 1:length(files)){
-  print(names(files[[i]]))
-  if(ncol(files[[i]]) <= 3){
-    colnames(files[[i]]) <- column_names
-    }
-  print(names(files[[i]]))
-}
-
-# Force all columns to type of character
-for(i in 1:length(files)){
-  files[[i]] <- files[[i]] %>% mutate_all(as.character)
-}
-
+# Load to DSS ----
 con <- LICHospitalR::db_connect()
 for(i in 1:length(files)){
   file_to_dss_name <- paste0("c_nysdoh_sepsis_", names(files[i]))
